72fc8bdf46b0a235076e9c88eaf2e6cfcbcfab70
Lucas Récan <76816773+Ribouu@users.noreply.github.com>
2025-11-28 11:16:44 +0100

    ébauche DAO

--- src/modele/DAO.php
+++ src/modele/DAO.php
@@ -1,1 +1,19 @@
+
+<?php
 // accès la base de données via ce fichier uniquement
+
+// ---- fonctions pour les joueurs ----
+function ajouterJoueur($nom, $prenom, $num_licence, $date_naissance, $taille, $poids, $statut) {};
+
+// ...
+
+// ---- fonctions pour les rencontres ----
+function ajouterRencontre($date_rencontre, $heure, $adresse, $nom_equipe_adverse, lieu, resultat) {};
+
+// ...
+
+// ---- fonctions pour les commentaires ----
+function ajouterCommentaire($commentaire, $date_commentaire) {};
+
+// ...
+?>
