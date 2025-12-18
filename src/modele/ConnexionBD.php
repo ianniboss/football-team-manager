3d31ac185c23519a9e273ebc7c8307d5dc86e623
lebeaugs <ianhafiz9999@gmail.com>
2025-12-18 17:23:27 +0100

    modifier le connexiionbd pour integrer entre local et infinityfree

--- src/modele/ConnexionBD.php
+++ src/modele/ConnexionBD.php
@@ -1,14 +1,28 @@
 <?php
-class ConnexionBD {
+class ConnexionBD
+{
     private static $instance = null;
     private $pdo;
 
     private function __construct()
     {
-        $host = 'sql100.infinityfree.com';
-        $db = 'if0_40713656_ftm';
-        $user = 'if0_40713656';
-        $pass = 'EJ8uoCbnPqYCxl';
+        // Detect if running locally or on InfinityFree
+        $isLocal = ($_SERVER['SERVER_NAME'] ?? 'localhost') === 'localhost'
+            || ($_SERVER['SERVER_ADDR'] ?? '127.0.0.1') === '127.0.0.1';
+
+        if ($isLocal) {
+            // Local development (Laragon/XAMPP)
+            $host = '127.0.0.1';
+            $db = 'ftm-projet';
+            $user = 'root';
+            $pass = '';
+        } else {
+            // InfinityFree deployment
+            $host = 'sql100.infinityfree.com';
+            $db = 'if0_40713656_ftm';
+            $user = 'if0_40713656';
+            $pass = 'EJ8uoCbnPqYCxl';
+        }
 
         try {
             $this->pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
