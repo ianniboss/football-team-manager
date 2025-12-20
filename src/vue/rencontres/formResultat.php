298ff7ee55661cc6ca7168f2e8339d7c01e38476
lebeaugs <ianhafiz9999@gmail.com>
2025-12-20 23:41:07 +0100

    mise a jour pour qu'il utilise les images des players

--- src/vue/rencontres/formResultat.php
+++ src/vue/rencontres/formResultat.php
@@ -348,9 +348,15 @@
                     <?php foreach ($joueursFeuille as $j): ?>
                         <div class="player-eval-row">
                             <div class="player-info">
-                                <div class="player-avatar">
-                                    <?= strtoupper(substr($j['prenom'], 0, 1) . substr($j['nom'], 0, 1)) ?>
-                                </div>
+                                <?php if (!empty($j['image'])): ?>
+                                    <img src="/modele/img/players/<?php echo htmlspecialchars($j['image']); ?>" 
+                                         alt="Photo de <?php echo htmlspecialchars($j['prenom']); ?>"
+                                         style="width: 45px; height: 45px; border-radius: 50%; object-fit: cover; border: 2px solid #e0e0e0;">
+                                <?php else: ?>
+                                    <div class="player-avatar">
+                                        <?= strtoupper(substr($j['prenom'], 0, 1) . substr($j['nom'], 0, 1)) ?>
+                                    </div>
+                                <?php endif; ?>
                                 <div>
                                     <div class="player-name"><?php echo htmlspecialchars($j['prenom'] . ' ' . $j['nom']); ?></div>
                                     <div class="player-role"><?= $j['titulaire'] ? 'Titulaire' : 'Remplaçant' ?></div>
