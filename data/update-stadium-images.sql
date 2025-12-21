52d1f2c300e29d824e229397def21f3528243643
lebeaugs <ianhafiz9999@gmail.com>
2025-12-21 12:29:21 +0100

    supprimer les anciens BD et exporter celui de derniere version

--- data/update-stadium-images.sql
+++
@@ -1,7 +0,0 @@
--- Update stadium images for matches
--- Match the stadium name in address to the image filename
-
-UPDATE rencontre SET image_stade = 'stade_municipal.avif' WHERE adresse LIKE '%Stade Municipal%';
-UPDATE rencontre SET image_stade = 'parc_des_princes.jpeg' WHERE adresse LIKE '%Parc des Princes%';
-UPDATE rencontre SET image_stade = 'stade_velodrome.png' WHERE adresse LIKE '%Vélodrome%';
-UPDATE rencontre SET image_stade = 'camp_nou.png' WHERE adresse LIKE '%Camp Nou%';
